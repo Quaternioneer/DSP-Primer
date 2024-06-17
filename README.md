857c96785fc7acb3a133acc0717d7445b8116a8f
Quaternioneer <nat.admin@protonmail.com>
2024-06-16 19:14:35 -0700

    added a header and changed verbosity

---
+++ README.md
@@ -0,0 +1,2 @@
+2024 June 16: version 0.1
+I added a header row to data, and changed the verbosity to 2. I piped the screen output to screen.txt. The frequency range from 0 to 1 Hz is below the human hearing threshold of 20 Hz. I might sweep the human hearing range in the next version. Note that w=2PIf, where w is omega, the angular frequency.
--- fft.c
+++ fft.c
@@ -7,13 +7,13 @@ Cooley, Lewis, and Welch; from Rabiner & Gold (1975)*/
 int main() {
 FILE *fopen(), *fdata;
 #define dB(x) (((fabs(x))>(0.0))?(20.*log10(fabs(x))):(-1000.))
-#define verbose 0  /* 2 = print input and output */
+#define verbose 2  /* 2 = print input and output */
 #define N 1024     /* length of transform */
 #define M 10       /* log N */
 #define M_PI    3.14159265358979323846
 int i, j, k, L;    /* indexes */
 int LE, LE1, ip;
-int NV2, NM1;
+int NV2, NM1;	   //N divided by 2 and N minus 1
 double ar[N], ai[N]; /* array of points */
 double t;            /* temp */
 double Ur, Ui, Wr, Wi, Tr, Ti;  //r denotes real and i stands for imaginary
@@ -83,7 +83,7 @@ for (L=1; L<=M; L++)    /* stage L */
 
    { for (i=j; i<=N; i+=LE) /* butterfly */
       { ip = i+LE1;
-        Tr = ar[ip-1]*Ur-ai[ip-1]*Ui;     // T = a*U where a and U are complex
+        Tr = ar[ip-1]*Ur-ai[ip-1]*Ui;     // T = a*U where a=e^(i*theta) and U is complex
         Ti = ar[ip-1]*Ui+ai[ip-1]*Ur;
         ar[ip-1] = ar[i-1] - Tr;          // a(ip-1) = a(i-1)-T where LE = wikipedia's m OR a(k+j) = u - T source Wikipedia
         ai[ip-1] = ai[i-1] - Ti;          //Note: ip = i+m/2 (wikipedia) AND s (Wikipedia) =L
@@ -107,6 +107,7 @@ if (verbose>=2)
     printf("%6.4lf ", ai[i]);
    printf("\n"); }
 
+fprintf(fdata,"Hz     magnitude     dB\n");
 for(i=0; i<N; i++)
  { mag = sqrt(ar[i]*ar[i]+ai[i]*ai[i]);
 fprintf(fdata,"%6.4lf %le %lf\n",(double)i/(double)N, mag, dB(mag));}
